9838ca4dc302010624fd4de0f5da2c6dd9052e3b
simon <info@simonvreman.nl>
2023-07-06 21:03:46 +0200

    Add call to action component

---
+++ src/components/CallToAction.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { Button } from 'carbon-components-svelte';
+
+	export let icon;
+	export let action;
+</script>
+
+<div class="flex items-center justify-center">
+	<Button {icon}>{action}</Button>
+</div>
