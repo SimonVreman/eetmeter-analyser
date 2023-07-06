58297c4cc17ffb4f5bb9bfcff98401b5db9a8d72
simon <info@simonvreman.nl>
2023-07-06 23:55:35 +0200

    Add components to import page

---
+++ src/routes/import/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+	import { Column, Grid, Row, Tile } from 'carbon-components-svelte';
+	import FileUpload from '../../components/import-files/FileUpload.svelte';
+	import ImportPreview from '../../components/import-files/ImportPreview.svelte';
+
+	let importedFiles = [];
+</script>
+
+<Grid>
+	<Row>
+		<Column>
+			<h1>Import data</h1>
+			<p class="big-paragraph">Add .xml exports from Mijn Eetmeter to analyse.</p>
+			<Grid>
+				<Row>
+					<Column>
+						<FileUpload bind:importedFiles />
+					</Column>
+					<Column>
+						<ImportPreview files={importedFiles} />
+					</Column>
+				</Row>
+			</Grid>
+		</Column>
+	</Row>
+</Grid>
