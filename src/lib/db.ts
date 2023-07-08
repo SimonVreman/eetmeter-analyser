ee3ac958e6aedb550636472fb71ed877e15d57c7
simon <info@simonvreman.nl>
2023-07-08 20:09:34 +0200

    Define schema for indexed db

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,15 +1,65 @@
 import Dexie, { type Table } from 'dexie';
-import type { Consumption } from '../types/schema';
+import type {
+	Consumption,
+	ConsumptionNutrient,
+	Nutrient,
+	Product,
+	ProductNutrient
+} from '../types/schema';
+import { NutrientType } from '../types/schema';
 
 export class LocalDatabase extends Dexie {
+	nutrients: Table<Nutrient>;
 	consumptions!: Table<Consumption>;
+	consumptionNutrients!: Table<ConsumptionNutrient>;
+	products!: Table<Product>;
+	productNutrients!: Table<ProductNutrient>;
 
 	constructor() {
 		super('eetmeterdb');
 		this.version(1).stores({
-			consumptions: '++id, consumedAt, product'
+			nutrients: '++id, name, unit',
+			consumptions: '++id, date, period, productId, grams',
+			consumptionNutrients: '++id, nutrientId, type, amount',
+			products: 'id, name, nevo, brand',
+			productNutrients: '++id, nutrientId, type, per100Gram'
 		});
 	}
 }
 
 export const db = new LocalDatabase();
+
+db.on('populate', () =>
+	db.nutrients.bulkAdd([
+		{ id: NutrientType.ENERGY, name: 'Energy', unit: 'kcal' },
+		{ id: NutrientType.FAT, name: 'Fat', unit: 'g' },
+		{ id: NutrientType.SATURATED_FAT, name: 'Saturated fat', unit: 'g' },
+		{ id: NutrientType.CARBOHIDRATES, name: 'Carbohydrates', unit: 'g' },
+		{ id: NutrientType.PROTEIN, name: 'Protein', unit: 'g' },
+		{ id: NutrientType.FIBER, name: 'Fiber', unit: 'g' },
+		{ id: NutrientType.SALT, name: 'Salt', unit: 'g' },
+		{ id: NutrientType.ALCOHOL, name: 'Alcohol', unit: 'g' },
+		{ id: NutrientType.WATER, name: 'Water', unit: 'g' },
+		{ id: NutrientType.SODIUM, name: 'Sodium', unit: 'mg' },
+		{ id: NutrientType.POTASSIUM, name: 'Potassium', unit: 'mg' },
+		{ id: NutrientType.CALCIUM, name: 'Calcium', unit: 'mg' },
+		{ id: NutrientType.MAGNESIUM, name: 'Magnesium', unit: 'mg' },
+		{ id: NutrientType.IRON, name: 'Iron', unit: 'mg' },
+		{ id: NutrientType.SELENIUM, name: 'Selenium', unit: 'µg' },
+		{ id: NutrientType.ZINC, name: 'Zinc', unit: 'mg' },
+		{ id: NutrientType.VITAMIN_A, name: 'Vitamin A', unit: 'µg' },
+		{ id: NutrientType.VITAMIN_B1, name: 'Vitamin B1', unit: 'mg' },
+		{ id: NutrientType.VITAMIN_B2, name: 'Vitamin B2', unit: 'mg' },
+		{ id: NutrientType.VITAMIN_B6, name: 'Vitamin B6', unit: 'mg' },
+		{ id: NutrientType.VITAMIN_B12, name: 'Vitamin B12', unit: 'µg' },
+		{ id: NutrientType.VITAMIN_C, name: 'Vitamin C', unit: 'mg' },
+		{ id: NutrientType.VITAMIN_D, name: 'Vitamin D', unit: 'µg' },
+		{ id: NutrientType.VITAMIN_E, name: 'Vitamin E', unit: 'mg' },
+		{ id: NutrientType.FOLIC_ACID, name: 'Folic acid', unit: 'µg' },
+		{ id: NutrientType.NICOTINIC_ACID, name: 'Nicotinic acid', unit: 'mg' },
+		{ id: NutrientType.IODINE, name: 'Iodine', unit: 'µg' },
+		{ id: NutrientType.PHOSPHORUS, name: 'Phosphorus', unit: 'mg' },
+		{ id: NutrientType.SUGARS, name: 'Sugars', unit: 'g' },
+		{ id: NutrientType.PLANT_PROTEIN, name: 'Plant protein', unit: 'g' }
+	])
+);
--- src/types/schema.ts
+++ src/types/schema.ts
@@ -1,5 +1,75 @@
 export interface Consumption {
 	id?: number;
-	consumedAt: Date;
-	productCode: string;
+	date: Date;
+	period: ConsumptionPeriod;
+	productId: string;
+	grams: number;
+}
+
+export enum ConsumptionPeriod {
+	BREAKFAST = 'BREAKFAST',
+	LUNCH = 'LUNCH',
+	DINNER = 'DINNER',
+	OTHER = 'OTHER'
+}
+
+export interface ConsumptionNutrient {
+	id?: number;
+	nutrientId: number;
+	type: NutrientType;
+	amount: number;
+}
+
+export interface Product {
+	id?: string;
+	name: string;
+	nevo?: number;
+	brand?: string;
+}
+
+export interface ProductNutrient {
+	id?: number;
+	name: string;
+	nutrientId: number;
+	type: NutrientType;
+	per100Gram: number;
+}
+
+export interface Nutrient {
+	id?: number;
+	name: string;
+	unit: string;
+}
+
+export enum NutrientType {
+	ENERGY = 1,
+	FAT = 2,
+	SATURATED_FAT = 3,
+	CARBOHIDRATES = 4,
+	PROTEIN = 5,
+	FIBER = 5,
+	SALT = 6,
+	ALCOHOL = 7,
+	WATER = 8,
+	SODIUM = 9,
+	POTASSIUM = 10,
+	CALCIUM = 11,
+	MAGNESIUM = 12,
+	IRON = 13,
+	SELENIUM = 14,
+	ZINC = 15,
+	VITAMIN_A = 16,
+	VITAMIN_D = 17,
+	VITAMIN_E = 18,
+	VITAMIN_B1 = 19,
+	VITAMIN_B2 = 20,
+	VITAMIN_B6 = 21,
+	FOLIC_ACID = 22,
+	VITAMIN_B12 = 23,
+	NICOTINIC_ACID = 24,
+	VITAMIN_C = 25,
+	IODINE = 26,
+	PHOSPHORUS = 27,
+	SUGARS = 28,
+	PLANT_PROTEIN = 29
 }
