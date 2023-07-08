6a5b9121d361a7dee1b8788781a2590d83bae95d
simon <info@simonvreman.nl>
2023-07-08 19:29:11 +0200

    Create explore page

---
+++ src/routes/explore/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { Column, Content, Grid, Row, Tab, TabContent, Tabs } from 'carbon-components-svelte';
+	import ConsumptionTable from '../../components/tables/ConsumptionTable.svelte';
+</script>
+
+<Content class="flex">
+	<Grid class="w-full shrink">
+		<Row>
+			<Column>
+				<h1 class="mb-8">Explore data</h1>
+				<Tabs type="container">
+					<Tab label="Consumptions" />
+					<Tab label="Products" />
+					<svelte:fragment slot="content">
+						<TabContent><ConsumptionTable /></TabContent>
+						<TabContent>products</TabContent>
+					</svelte:fragment>
+				</Tabs>
+			</Column>
+		</Row>
+	</Grid>
+</Content>
