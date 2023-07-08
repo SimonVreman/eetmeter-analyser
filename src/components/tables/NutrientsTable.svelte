c77f79e4b4e3e90944e32de0e2f5a2ca0b4cd782
simon <info@simonvreman.nl>
2023-07-08 21:12:43 +0200

    Add nutrients table

---
+++ src/components/tables/NutrientsTable.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { db } from '../../lib/db';
+	import ExploreTable from './ExploreTable.svelte';
+</script>
+
+<ExploreTable
+	collection={db.nutrients}
+	title="Nutrients"
+	description="Available nutrient types, predefined."
+	headers={[
+		{ key: 'id', value: 'ID' },
+		{ key: 'name', value: 'Name' },
+		{ key: 'unit', value: 'Unit' }
+	]}
+/>
--- src/routes/explore/+page.svelte
+++ src/routes/explore/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { Column, Content, Grid, Row, Tab, TabContent, Tabs } from 'carbon-components-svelte';
 	import ConsumptionTable from '../../components/tables/ConsumptionTable.svelte';
+	import NutrientsTable from '../../components/tables/NutrientsTable.svelte';
 </script>
 
 <Content class="flex">
@@ -11,9 +12,11 @@
 				<Tabs type="container">
 					<Tab label="Consumptions" />
 					<Tab label="Products" />
+					<Tab label="Nutrients" />
 					<svelte:fragment slot="content">
 						<TabContent><ConsumptionTable /></TabContent>
 						<TabContent>products</TabContent>
+						<TabContent><NutrientsTable /></TabContent>
 					</svelte:fragment>
 				</Tabs>
 			</Column>
