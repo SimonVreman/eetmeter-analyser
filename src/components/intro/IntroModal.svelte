c0b8fd515298af1bdf730622ddc680dce4b3eee6
simon <info@simonvreman.nl>
2023-07-14 10:14:21 +0200

    Add loading state to welcome modal

--- src/components/intro/IntroModal.svelte
+++ src/components/intro/IntroModal.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { Loading, Modal } from 'carbon-components-svelte';
+	import { InlineLoading, Loading, Modal } from 'carbon-components-svelte';
 	import { demo, newUser } from '../../stores/demo';
 	import { enterDemoMode } from '../../lib/demo';
 	import { clearChartCache } from '../../stores/charts';
@@ -35,11 +35,18 @@
 	modalHeading="Welcome!"
 	primaryButtonText="Demo mode"
 	secondaryButtonText="Cancel"
+	primaryButtonDisabled={loading}
+	preventCloseOnClickOutside={loading}
 	on:click:button--secondary={close}
 	on:submit={enterDemo}
 >
-	<p>
-		Do you want to try the dashboard with some demo data first? You can always toggle demo mode
-		using the settings menu at the top right. You can also enable dark mode there ;)
-	</p>
+	<div class="relative">
+		<p class="pb-8">
+			Do you want to try the dashboard with some demo data first? You can always toggle demo mode
+			using the settings menu at the top right. You can also enable dark mode there ;)
+		</p>
+		{#if loading}
+			<InlineLoading class="absolute bottom-0" description="Loading demo data..." />
+		{/if}
+	</div>
 </Modal>
